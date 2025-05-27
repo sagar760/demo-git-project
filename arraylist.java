15966abb02a02a4c344674099c9ecd4de7b91d04
sagar@760 <sagarsahoo0120@gmail.com>
2025-05-27 22:30:19 +0530

    arraylist java code

---
+++ arraylist.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class arraylist{
+    public static void main(String[] args) {
+        ArrayList<Integer> li = new ArrayList<>();
+        for(int i = 0;i<5;i++){
+            li.add(i);
+        }
+        System.out.println(li);
+    }
+}
